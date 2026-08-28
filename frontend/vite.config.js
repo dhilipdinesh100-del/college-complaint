00fa3c942b9dfafbaf72c8d7a3f00681edc3d265
dhilipdinesh100-del <dhilipdinesh100@gmail.com>
2026-08-28 18:49:28 +0530

    Deploy frontend to GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 
-  // Required because the app is hosted at:
-  // https://dhilipdinesh100-del.github.io/college-complaint/
   base: '/college-complaint/',
 
   server: {
