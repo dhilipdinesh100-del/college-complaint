1e59dcd30250e1347bf8eb7e12ff6027b728ca84
dhilipdinesh100-del <dhilipdinesh100@gmail.com>
2026-08-29 11:39:44 +0530

    Fix production deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,6 +32,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
   'http://127.0.0.1:5173',
+  'https://dhilipdinesh100-del.github.io',
   frontendUrl,
 ].filter(Boolean);
 
